74ec8c32907c554a137209b800d5f51a333ad89f
IlyaDenisovichh <134489466+IlyaDenisovichh@users.noreply.github.com>
2023-08-02 19:05:09 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,23 @@
+describe('ToDoList', () => {
+   it('By iphone', () => {
+      cy.visit("https://forhemer.github.io/React-Todo-List/");
+      cy.get('.input-text').type('Первая заметка');
+      cy.get('.input-submit').click();
+      cy.get('.input-text').type('Вторая заметка');
+      cy.get('.input-submit').click();
+      cy.get('.input-text').type('Третья заметка');
+      cy.get('.input-submit').click();
+      //cy.wait(10000);
+      cy.get('li').should('have.length', 3);
+      cy.get(':nth-child(1) > .TodoItem_checkbox__Tf0FQ').click();
+      cy.get(':nth-child(1) > span').should('have.css', 'text-decoration', 'line-through solid rgb(89, 89, 89)');
+      cy.get('ul > :nth-child(3) > button').click();
+      cy.get('ul > :nth-child(3)').should('not.exist');
+
+
+
+
+
+
+   })
+}) 
